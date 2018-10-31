a28c6e1c7b9cf6dbf99cd4f8a7983887030fc36e
Alec Pattin <apattin03@gmail.com>
2018-10-31 18:55:14 -0500

    removed unused functions

--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -77,21 +77,6 @@ export function getMovie(id) {
 }
 
 
-export function saveMovie(movie) {
-  let movieInDb = movies.find(m => m._id === movie._id) || {};
-  movieInDb.name = movie.name;
-  movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-  movieInDb.numberInStock = movie.numberInStock;
-  movieInDb.dailyRentalRate = movie.dailyRentalRate;
-
-  if (!movieInDb._id) {
-    movieInDb._id = Date.now();
-    movies.push(movieInDb);
-  }
-
-  return movieInDb;
-}
-
 export function deleteMovie(id) {
   let movieInDb = movies.find(m => m._id === id);
   movies.splice(movies.indexOf(movieInDb), 1);
